ebba113d7e2157c01a710234bae191416a527628
SDEdward <edwardstrowaway@gmail.com>
2025-10-11 01:49:04 +0300

    you can now clear the list

--- _io_test/src/main.rs
+++ _io_test/src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut action = String::new();
     let mut list = String::new();
     let mut mistakes = 0;
+    let mut cleared = false;
 
     loop {
         println!("Select an action:");
@@ -26,7 +27,7 @@ fn main() {
                 wait_for_enter();
             }
             "2" => {
-                println!("\nEnter new list item (or type 'back' to return):");
+                println!("\nEnter new list item (or type 'back' to return or 'cls' to clear):");
                 let mut item = String::new();
                 io::stdin()
                     .read_line(&mut item)
@@ -34,15 +35,24 @@ fn main() {
                 let item = item.trim(); //makes, reads and trims the new item
 
                 if item == "back" {
+                    println!();
                     continue; // if the user types back then continue (programming teaches you to not trust language)
-                } else {
-                    if !list.is_empty() {
-                        list.push('\n'); // if the list isnt empty then adds a newline (notice its a push)
-                    } // ? change the above code and implement into the lower one if u like it more
+                } else if item == "cls" {
+                    list.clear();
+                    cleared = true;
+                } else if !list.is_empty() {
+                    list.push('\n'); // if the list isnt empty then adds a newline (notice its a push)
+                }
+                if !cleared {
                     list.push_str(item); // here then it adds the item (notice its a push_str)
                     println!("\nItem added to the list.\n");
+                } else {
+                    cleared = false;
+                    println!();
+                    continue;
                 }
             }
+
             "3" => break, // if its 3 then it exits
             _ => {
                 if mistakes == 0 {
@@ -62,7 +72,9 @@ fn main() {
                     println!("\nfuck you, {mistakes} mistakes\n");
                     wait_for_enter();
                 } else if 9 == mistakes || mistakes < 9 {
-                    println!("\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n STOP FUCKING SPAMMING.\n"); // "clears screen" and yells (this is easier the ncatually clearing the screen cuz os's decided to not be the same)
+                    println!(
+                        "\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n STOP FUCKING SPAMMING.\n"
+                    ); // "clears screen" and yells (this is easier the ncatually clearing the screen cuz os's decided to not be the same)
                     break; // the program gets annoyed and kills itself lol
                 }
             } // if the user says anything else then they get flamed the worse the more mistakes thay make
