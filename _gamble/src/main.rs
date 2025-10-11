9a8b2b4f46a203a55eaa756e887b6078ec7af942
SDEdward <edwardstrowaway@gmail.com>
2025-10-11 21:10:26 +0300

    saving ts rq

--- _gamble/src/main.rs
+++ _gamble/src/main.rs
@@ -1,4 +1,4 @@
-use rand::rng;
+use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
@@ -71,10 +71,60 @@ fn bj() {
         }
     }
 }
+
 fn bjgame() {
-    let mut dealer: u8 = rand::rng().gen_range(2..=21);
-    let mut player: u8 = rand::rng().gen_range(2..=21);
+    let mut dealer: u8 = rand::rng().random_range(2..=21);
+    let mut player: u8 = rand::rng().random_range(2..=21);
+    dealer -= 1;
+    let dealer_deduction: u8 = rand::rng().random_range(1..=dealer);
+    dealer += 1;
+    let fake_dealer: u8 = dealer - dealer_deduction;
+
+    println!("Dealer has {fake_dealer} showing (1 card hidden)");
+    println!("You have {player}\n");
+    println!("Do you:");
+    println!("1: Stand");
+    println!("2: Hit");
+
+    loop {
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Idk how u made this panic man wtf!?!??");
+
+        let choice: u8 = match choice.trim().parse() {
+            Ok(num) => num, // remember that num here can be anythiung ( its a temporary variable )
+            Err(_) => {
+                println!("NUMBER MASON NUMBER!!!!! (less than 256 btw)");
+                continue;
+            }
+        };
+
+        match choice {
+            1 => {
+                println!("You stand with {player}. Dealer reveals hidden card...");
+                println!("Dealer's total is {dealer}.");
+                // todo Add win/loss logic here
+            }
+            2 => {
+                let hit_card: u8 = rand::rng().random_range(1..=11);
+                player += hit_card;
+                println!("You drew a {hit_card}. Your total is now {player}.");
+                if player > 21 {
+                    println!("You bust! Dealer wins.");
+                    // todo enter to go back to the bj main menu
+                } else {
+                    // todo same as the comment above
+                }
+            }
+            _ => {
+                println!("Invalid choice. Try 1 or 2.");
+                continue;
+            }
+        }
+    }
 }
+
 fn slot() {}
 
 fn roulette() {}
